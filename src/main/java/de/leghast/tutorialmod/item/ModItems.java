fdb38b550ca7e25b3d31a8e5c9d8c825b1b1e458
GhastCraftHD <70332776+LeGhast@users.noreply.github.com>
2022-09-26 16:48:24 +0200

    added crafting recipes

--- src/main/java/de/leghast/tutorialmod/item/ModItems.java
+++ src/main/java/de/leghast/tutorialmod/item/ModItems.java
@@ -3,20 +3,19 @@ package de.leghast.tutorialmod.item;
 import de.leghast.tutorialmod.TutorialMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
 
     public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
 
     public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
 
     private static Item registerItem(String name, Item item)
     {
